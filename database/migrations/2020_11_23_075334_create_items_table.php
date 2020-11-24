6feff6fd9a6dd46da53ea567600ff880a499808e
Tanmaya0505 <tanmayarout54@gmail.com>
2020-11-24 17:20:09 +0530

    migrate table

---
+++ database/migrations/2020_11_23_075334_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('itemcategory_id')->unsigned();
+            $table->string('name', 127);
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->foreign('itemcategory_id')->references('id')->on('itemcategory')->onDelete('cascade')->onUpdate('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
